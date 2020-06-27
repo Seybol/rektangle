45dcf6cb049d92f95373e7a0b149f330bcd29d40
Sébastien CEBULA <sebastien.cebula@gmail.com>
2020-06-27 21:18:22 +0200

    Refactored main rektangle class

--- rektangle/rektangle.rb
+++ rektangle/rektangle.rb
@@ -1,51 +1,19 @@
-class Rektangle
-  DEFAULT_SHAPES = %w[A B C D .]
+require_relative 'middle_lines.rb'
+require_relative 'bottom_line.rb'
+require_relative 'top_line.rb'
 
-  def shapes_valid?
-    @shapes.length == 5
-  end
+class Rektangle
+  include MiddleLines
+  include BottomLine
+  include TopLine
+  DEFAULT_SHAPES = %w[A B C D .]
 
   def initialize(width, height, *shapes)
     @width = width
     @height = height
     @shapes = shapes
-    @shapes = shapes_valid? ? shapes : DEFAULT_SHAPES
+    @shapes = shapes.empty? ? DEFAULT_SHAPES : shapes
     @top_left, @top_right, @bottom_right, @bottom_left, @border = @shapes
-  end
-
-  def draw_top_line
-    top_line = ''
-    top_line << @top_left
-    return top_line + "\n" if @width == 1
-    (@width - 2).times do
-      top_line << @border
-    end
-    top_line << "#{@top_right}\n"
-  end
-
-  def draw_bottom_line
-    bottom_line = ''
-    bottom_line << @bottom_left
-    return bottom_line + "\n" if @width == 1
-    (@width - 2).times do
-      bottom_line << @border
-    end
-    bottom_line << "#{@bottom_right}\n"
-  end
-
-  def draw_middle_line
-    return "#{@border}\n" if @width == 1
-    middle_of_line = ' ' * (@width - 2)
-    "#{@border}" + middle_of_line + "#{@border}\n"
-  end
-
-  def draw_middle_lines
-    middle_lines = ''
-    (@height - 2).times do
-      line = draw_middle_line
-      middle_lines << line
-    end
-    middle_lines
   end
 
   def draw
