0414bd9fd77e1caf9c0aeca65b3b12ad9194cd63
Sébastien CEBULA <sebastien.cebula@gmail.com>
2020-06-27 21:44:13 +0200

    Added defaults value in help menu

--- rektangle.rb
+++ rektangle.rb
@@ -4,11 +4,11 @@ Dir[File.dirname(__FILE__ ) + '/errors/*.rb'].each { |file| require file }
 def help_menu
   puts '~~Help Menu~~'
   puts 'Usage: ruby file_name width height [optional: shape_1 shape2 shape_3 shape_4 shape_5]'
-  puts 'shape_1: top left character'
-  puts 'shape_2: top right character'
-  puts 'shape_3: bottom right character'
-  puts 'shape_4: bottom left character'
-  puts 'shape_5: border character'
+  puts 'shape_1: top left character (Default: A)'
+  puts 'shape_2: top right character (Default: B)'
+  puts 'shape_3: bottom right character (Default: C)'
+  puts 'shape_4: bottom left character (Default: D)'
+  puts 'shape_5: border character (Default: .)'
 end
 
 begin
@@ -17,29 +17,21 @@ begin
     puts help_menu
     abort
   end
+
+  # Parse Arguments
   width = ARGV[0].to_i
   height = ARGV[1].to_i
-
-  if width < 1 || height < 1
-    raise ArgumentSignError
-  end
-
-  if width > 9000 || height > 9000
-    raise ArgumentTooBigError
-  end
   shapes = ARGV[2..6]
-  shapes.each do |shape|
-    raise ShapeLengthIncorrectError if shape.length != 1
-  end
-  if shapes.empty? || shapes.length == 5
-    rektangle = Rektangle.new(width, height)
-    puts rektangle.draw
-  else
-    puts 'hello'
-    raise ShapesNumberIncorrectError
-  end
+
+  # Error Handling
+  raise ArgumentSignError if width < 1 || height < 1
+  raise ArgumentTooBigError if width > 9000 || height > 9000
+  shapes.each { |shape| raise ShapeLengthIncorrectError if shape.length != 1 }
+  raise ShapesNumberIncorrectError unless shapes.empty? || shapes.length == 5
+
+  rektangle = Rektangle.new(width, height)
+  puts rektangle.draw
 rescue ArgumentError => e
   puts "#{e.class} -> #{e.message}"
   puts help_menu
 end
-# if ok -> draw
